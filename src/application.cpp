73dc51018ec5a0e2936980446910ff1500a78bf5
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2025-01-04 12:02:27 +0300

    cleanup

--- src/application.cpp
+++ src/application.cpp
@@ -2,25 +2,21 @@
 
 Application::Application() : Shared::Application("hl_bsp_viewer", { Flag::Scene })
 {
+	CONSOLE->execute("hud_show_fps 1");
 	PLATFORM->setTitle(PRODUCT_NAME);
 
-	mCamera = std::make_shared<skygfx::utils::PerspectiveCamera>();
-	mCameraController = std::make_shared<Shared::FirstPersonCameraController>(mCamera);
+	mViewport3D = std::make_shared<Scene::Viewport3D>();
+	getScene()->getRoot()->attach(mViewport3D);
 
-	mCamera->yaw = glm::radians(90.0f);
-	mCamera->position = { 0.0f, 0.0f, -300.0f };
-	mCamera->world_up = { 0.0f, -1.0f, 0.0f };
-
+	mCameraController = std::make_shared<Shared::FirstPersonCameraController>(mViewport3D->getCamera());
 	mCameraController->setSensivity(1.0f);
 	mCameraController->setSpeed(5.0f);
 
-	CONSOLE->execute("hud_show_fps 1");
+	//mBspFile.loadFromFile("de_dust2.bsp", true);
+	mBspFile.loadFromFile("cs_assault.bsp", true);
 
-	//mBSPFile.loadFromFile("de_dust2.bsp", true);
-	mBSPFile.loadFromFile("cs_assault.bsp", true);
-
-	auto& textures = mBSPFile.getTextures();
-	auto& wads = mBSPFile.getWADFiles();
+	auto& textures = mBspFile.getTextures();
+	auto& wads = mBspFile.getWADFiles();
 	std::unordered_map<TexId, std::shared_ptr<skygfx::Texture>> textures_map;
 
 	for (int tex_id = 0; tex_id < textures.size(); tex_id++)
@@ -90,216 +86,70 @@ Application::Application() : Shared::Application("hl_bsp_viewer", { Flag::Scene 
 		}
 	}
 
-	auto model_matrix = glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(1, 0, 0));
-
-	mBspDraw = std::make_shared<HL::BspDraw>(mBSPFile, textures_map, model_matrix);
-
-	GRAPHICS->setBatching(false); // bug in batching, 3D objects cannot be rendered
-
-	//mCamera->setPosition({ 359.867f, -368.278f, 1721.268f });
-	mCamera->position = { 1441.438f, -61.265f, 802.192f };
-
-	auto lights = mBspDraw->getLights();
-
-	lights.push_back(skygfx::utils::DirectionalLight{
-		.direction = { 0.75f, 0.75f, 0.75f },
-		//.ambient = Graphics::Color::ToNormalized(23, 22, 22),
-		//.diffuse = Graphics::Color::ToNormalized(65, 65, 65),
-		//.specular = Graphics::Color::ToNormalized(54, 51, 51),
-		.ambient = Graphics::Color::ToNormalized(116, 103, 103),
-		.diffuse = Graphics::Color::ToNormalized(139, 176, 147),
-		.specular = Graphics::Color::ToNormalized(255, 255, 255),
-	});
-
-	lights.push_back(skygfx::utils::PointLight{
-		.position = { 151.977f, -326.671f, 1629.473f },
-		.ambient = { 1.0f, 1.0f, 1.0f },
-		.diffuse = { 0.25f, 1.0f, 0.25f },
-		.specular = { 0.25f, 0.25f, 1.0f },
-		.constant_attenuation = 0.0f,
-		.linear_attenuation = 0.012f,
-		.quadratic_attenuation = 0.0f
-	});
-
-	mBspDraw->setLights(lights);
-
-	//mCamera->setPitch(glm::radians(12.0f));
-	//mCamera->setYaw(glm::radians(-138.0f));
-	mCamera->pitch = glm::radians(-13.0f);
-	mCamera->yaw = glm::radians(155.0f);
-
-	// light animation
-
-	Actions::Run(Actions::Collection::RepeatInfinite([this] {
-		static const std::vector<glm::vec3> Waypoints = {
-			{ 420.0f, -340.0f, 1648.0f },
-			{ -656.0f, -340.0f, 1648.0f },
-			{ -649.0f, -371.0f, 2379.0f },
-			{ -549.0f, -379.0f, 2810.0f },
-			{ 384.0f, -392.0f, 2820.0f }
-		};
-
-		auto seq = Actions::Collection::MakeSequence();
-
-		for (auto waypoint : Waypoints)
-		{
-			seq->add(Actions::Collection::Interpolate(waypoint, 10.0f, mLightPosition));
-		}
-
-		return seq;
-	}));
-
-	mBloomLayer = std::make_shared<Scene::BloomLayer>();
-	mBloomLayer->setStretch(1.0f);
-	mBloomLayer->setBrightThreshold(2.0f);
-	getScene()->getRoot()->attach(mBloomLayer);
-
-	mSceneSprite = std::make_shared<Scene::Sprite>();
-	mSceneSprite->setTexture(mSceneTarget);
-	mSceneSprite->setStretch(1.0f);
-	mBloomLayer->attach(mSceneSprite);
+	mBspMapEntity = std::make_shared<HL::BspMapEntity>(mBspFile, textures_map);
+	mBspMapEntity->setRotation({ glm::radians(-90.0f), 0.0f, 0.0f });
+	mViewport3D->addEntity(mBspMapEntity);
 
 #if defined(PLATFORM_MAC)
 	PLATFORM->resize(1600, 1200);
 #endif
+
+	mTraceDotEntity = std::make_shared<Scene::SingleMeshEntity>();
+	mTraceDotEntity->setTopology(skygfx::Topology::LineList);
+	mViewport3D->addEntity(mTraceDotEntity);
+
+	std::vector<skygfx::utils::Mesh::Vertex> vertices;
+	for (int i = 0; i < 3; i++)
+	{
+		const glm::vec4 color = { Graphics::Color::Lime, 1.0f };
+
+		skygfx::utils::Mesh::Vertex v0, v1, v2;
+
+		v0.color = color;
+
+		v1.pos[i] = 10.0f;
+		v1.color = color;
+
+		v2.pos[i] = -10.0f;
+		v2.color = color;
+
+		vertices.push_back(v0);
+		vertices.push_back(v1);
+		vertices.push_back(v0);
+		vertices.push_back(v2);
+	}
+
+	mTraceDotEntity->setVertices(vertices);
 }
 
 void Application::onFrame()
 {
-	auto directionalLight = std::get<skygfx::utils::DirectionalLight>(mBspDraw->getLights().at(0));
-	auto pointLight = std::get<skygfx::utils::PointLight>(mBspDraw->getLights().at(1));
-
 	static bool show_settings = false;
 
 	ImGui::Begin("Settings", nullptr, ImGui::User::ImGuiWindowFlags_Overlay & ~ImGuiWindowFlags_NoInputs);
 	ImGui::SetWindowPos(ImGui::User::BottomLeftCorner());
 
+	auto camera = mViewport3D->getCamera();
+
 	if (show_settings)
 	{
-		ImGui::SliderAngle("Pitch##1", &mCamera->pitch, -89.0f, 89.0f);
-		ImGui::SliderAngle("Yaw##1", &mCamera->yaw, -180.0f, 180.0f);
-		ImGui::SliderFloat("Fov##1", &mCamera->fov, 1.0f, 180.0f);
-		ImGui::DragFloat3("Position##1", (float*)&mCamera->position);
-
-		ImGui::Separator();
-		ImGui::Text("Directional Light");
-		ImGui::DragFloat3("Direction##2", (float*)&directionalLight.direction, 0.1f, -1.0f, 1.0f);
-		ImGui::ColorEdit3("Ambient##2", (float*)&directionalLight.ambient);
-		ImGui::ColorEdit3("Diffuse##2", (float*)&directionalLight.diffuse);
-		ImGui::ColorEdit3("Specular##2", (float*)&directionalLight.specular);
-
-		ImGui::Separator();
-		ImGui::Text("Point Light");
-		ImGui::DragFloat3("Position##3", (float*)&pointLight.position);
-		ImGui::SameLine();
-		if (ImGui::Button("Here"))
-			pointLight.position = mCamera->position;
-		ImGui::DragFloat("Constant Attenuation##3", &pointLight.constant_attenuation, 0.01f);
-		ImGui::DragFloat("Linear Attenuation##3", &pointLight.linear_attenuation, 0.001f);
-		ImGui::DragFloat("Quadratic Attenuation##3", &pointLight.quadratic_attenuation, 0.0001f);
-		ImGui::ColorEdit3("Ambient##3", (float*)&pointLight.ambient);
-		ImGui::ColorEdit3("Diffuse##3", (float*)&pointLight.diffuse);
-		ImGui::ColorEdit3("Specular##3", (float*)&pointLight.specular);
-
-		ImGui::Separator();
-		ImGui::Checkbox("Light Animation", &mLightAnimation);
-
-		ImGui::Separator();
-
-		auto bloom_enabled = mBloomLayer->isPostprocessEnabled();
-		auto threshold = mBloomLayer->getBrightThreshold();
-		auto glow = mBloomLayer->getIntensity();
-
-		ImGui::Checkbox("Bloom Enabled", &bloom_enabled);
-		ImGui::SliderFloat("Bright Threshold", &threshold, 0.0f, 4.0f);
-		ImGui::SliderFloat("Glow Intensity", &glow, 0.0f, 8.0f);
-
-		mBloomLayer->setPostprocessEnabled(bloom_enabled);
-		mBloomLayer->setBrightThreshold(threshold);
-		mBloomLayer->setIntensity(glow);
+		ImGui::SliderAngle("Pitch##1", &camera->pitch, -89.0f, 89.0f);
+		ImGui::SliderAngle("Yaw##1", &camera->yaw, -180.0f, 180.0f);
+		ImGui::SliderFloat("Fov##1", &camera->fov, 1.0f, 180.0f);
+		ImGui::DragFloat3("Position##1", (float*)&camera->position);
+		ImGui::DragFloat3("WorldUp##1", (float*)&camera->world_up);
 	}
 
 	ImGui::Checkbox("Settings", &show_settings);
-
 	ImGui::End();
 
-	if (mLightAnimation)
-		pointLight.position = mLightPosition;
+	glm::vec3 pos = { camera->position.x, -camera->position.z, camera->position.y };
 
-	auto lights = mBspDraw->getLights();
-	lights[0] = directionalLight;
-	lights[1] = pointLight;
-	mBspDraw->setLights(lights);
+	auto vectors = skygfx::utils::MakePerspectiveCameraVectors(*camera);
 
-	mBspDraw->draw(mSceneTarget, mCamera->position, mCamera->yaw, mCamera->pitch, mCamera->fov, mCamera->world_up);
+	glm::vec3 dir = { vectors.front.x, -vectors.front.z, vectors.front.y };
 
-	//
-
-	glm::vec3 pos = {
-		mCamera->position.x,
-		mCamera->position.z,
-		-mCamera->position.y,
-	};
-
-	auto vectors = skygfx::utils::MakePerspectiveCameraVectors(*mCamera);
-
-	glm::vec3 dir = {
-		vectors.front.x,
-		vectors.front.z,
-		-vectors.front.y,
-	};
-
-	auto trace = mBSPFile.traceLine(pos, pos + (dir * 8192.0f));
-
-	skygfx::utils::Mesh::Vertex v1, v2, v3, v4, v5, v6, v7;
-
-	v1.pos.x = trace.endpos.x;
-	v1.pos.y = trace.endpos.y;
-	v1.pos.z = trace.endpos.z;
-
-	v2.pos = { v1.pos.x + 10, v1.pos.y,      v1.pos.z };
-	v3.pos = { v1.pos.x - 10, v1.pos.y,      v1.pos.z };
-	v4.pos = { v1.pos.x,      v1.pos.y + 10, v1.pos.z };
-	v5.pos = { v1.pos.x,      v1.pos.y - 10, v1.pos.z };
-	v6.pos = { v1.pos.x,      v1.pos.y,      v1.pos.z + 10 };
-	v7.pos = { v1.pos.x,      v1.pos.y,      v1.pos.z - 10 };
-
-	v1.color = { Graphics::Color::Lime, 1.0f };
-	v2.color = v1.color;
-	v3.color = v1.color;
-	v4.color = v1.color;
-	v5.color = v1.color;
-	v6.color = v1.color;
-	v7.color = v1.color;
-
-	skygfx::utils::Mesh::Vertices vertices = {
-		v1, v2,
-		v1, v3,
-		v1, v4,
-		v1, v5,
-		v1, v6,
-		v1, v7
-	};
-
-	auto matrices = skygfx::utils::MakeCameraMatrices(*mCamera);
-	auto model = glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(1, 0, 0));
-
-	GRAPHICS->begin();
-	GRAPHICS->pushRenderTarget(mSceneTarget);
-	GRAPHICS->pushProjectionMatrix(matrices.projection);
-	GRAPHICS->pushViewMatrix(matrices.view);
-	GRAPHICS->pushModelMatrix(model);
-	GRAPHICS->pushDepthMode(skygfx::ComparisonFunc::Less);
-	GRAPHICS->draw(nullptr, nullptr, skygfx::Topology::LineList, vertices, {});
-	GRAPHICS->pop(5);
-	GRAPHICS->end();
-}
-
-void Application::onEvent(const Platform::System::ResizeEvent& e)
-{
-	if (e.width == 0 && e.height == 0)
-		return;
-
-	mSceneTarget = std::make_shared<skygfx::RenderTarget>(e.width, e.height);
-	mSceneSprite->setTexture(mSceneTarget);
+	auto trace = mBspFile.traceLine(pos, pos + (dir * 8192.0f));
+	mTraceDotEntity->setEnabled(trace.fraction > 0.0f);
+	mTraceDotEntity->setPosition({ trace.endpos.x, trace.endpos.z, -trace.endpos.y });
 }
--- src/application.h
+++ src/application.h
@@ -4,9 +4,8 @@
 #include <HL/bsp_draw.h>
 #include <shared/all.h>
 
-class Application : public Shared::Application, 
-	public Common::FrameSystem::Frameable,
-	public Common::Event::Listenable<Platform::System::ResizeEvent>
+class Application : public Shared::Application,
+	public Common::FrameSystem::Frameable
 {
 public:
 	using TexId = int;
@@ -23,19 +22,11 @@ public:
 
 private:
 	void onFrame() override;
-	void onEvent(const Platform::System::ResizeEvent& e) override;
 
 private:
-	BSPFile mBSPFile;
-	std::shared_ptr<HL::BspDraw> mBspDraw;
-	std::shared_ptr<skygfx::utils::PerspectiveCamera> mCamera;
+	BSPFile mBspFile;
+	std::shared_ptr<Scene::Viewport3D> mViewport3D;
+	std::shared_ptr<HL::BspMapEntity> mBspMapEntity;
+	std::shared_ptr<Scene::SingleMeshEntity> mTraceDotEntity;
 	std::shared_ptr<Shared::FirstPersonCameraController> mCameraController;
-
-	bool mLightAnimation = false;
-	glm::vec3 mLightPosition;
-
-	std::shared_ptr<skygfx::RenderTarget> mSceneTarget = std::make_shared<skygfx::RenderTarget>(PLATFORM->getWidth(), PLATFORM->getHeight());
-
-	std::shared_ptr<Scene::BloomLayer> mBloomLayer;
-	std::shared_ptr<Scene::Sprite> mSceneSprite;
 };
